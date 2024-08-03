5aa4c15a699bf77cabed5751ca9c4dd569cfe18b
bryanmsombra27 <bryanjm96@gmail.com>
2024-08-03 11:15:10 -0600

    send qr only with admin authorization

--- src/app/pages/ver-transportistas/ver-transportistas.component.ts
+++ src/app/pages/ver-transportistas/ver-transportistas.component.ts
@@ -5,6 +5,7 @@ import {
 } from 'src/app/interfaces/mexcal/visitors.interface';
 import { AuthService } from 'src/app/services/auth.service';
 import { ErrorServidorService } from 'src/app/services/error-servidor.service';
+import { QRUDService } from 'src/app/services/qrud.service';
 import { VisitorsService } from 'src/app/services/visitors.service';
 
 @Component({
@@ -19,11 +20,15 @@ export class VerTransportistasComponent implements OnInit {
   busqueda: string = '';
   ocultarPaginacion: boolean = true;
   showSpinner: boolean = true;
+  msgQR: string = '';
+  existeMsgQRExito: boolean = false;
+  waitForAnswer: boolean = false;
 
   constructor(
     private VisitorsService: VisitorsService,
     private AuthService: AuthService,
-    private ErrorServidor: ErrorServidorService
+    private ErrorServidor: ErrorServidorService,
+    private QRUDService: QRUDService
   ) {}
 
   ngOnInit(): void {
@@ -54,6 +59,36 @@ export class VerTransportistasComponent implements OnInit {
         this.ErrorServidor.error();
       });
   }
+
+  enviarQR(id: any) {
+    this.waitForAnswer = true;
+    this.QRUDService.GenerarQRUSuario(id)
+      .then((data: any) => {
+        this.existeMsgQRExito = true;
+        this.msgQR = data.msg;
+        this.waitForAnswer = false;
+        setTimeout(() => {
+          this.existeMsgQRExito = false;
+        }, 2000);
+      })
+      .catch((err) => {
+        if (err.error.msg) {
+          this.msgQR = err.error.msg;
+          // this.existeQRregistrado = true;
+
+          setTimeout(() => {
+            // this.existeQRregistrado = false;
+          }, 2000);
+          return;
+        }
+        if (err.error.msgtk) {
+          this.AuthService.logout();
+          return;
+        }
+        this.ErrorServidor.error();
+      });
+  }
+
   nextPage() {
     this.getDrivers({
       page: this.page + 1,

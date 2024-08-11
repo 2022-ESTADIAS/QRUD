e305c9fff9bd706107df3050911087f5b4927ff5
bryanmsombra27 <bryanjm96@gmail.com>
2024-08-10 18:11:59 -0600

    adding translations for password change component

--- src/app/pages/cambio-contrasena/cambio-contrasena.component.ts
+++ src/app/pages/cambio-contrasena/cambio-contrasena.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { DynamicTranslationsService } from 'src/app/services/dynamic-translations.service';
 import { ErrorServidorService } from 'src/app/services/error-servidor.service';
 import { QRUDService } from 'src/app/services/qrud.service';
 import { StorageService } from 'src/app/services/storage.service';
@@ -11,36 +12,35 @@ import { StorageService } from 'src/app/services/storage.service';
 @Component({
   selector: 'app-cambio-contrasena',
   templateUrl: './cambio-contrasena.component.html',
-  styleUrls: ['./cambio-contrasena.component.css']
+  styleUrls: ['./cambio-contrasena.component.css'],
 })
 export class CambioContrasenaComponent implements OnInit {
-
   /**
    * propiedad que contiene el formulario reactivo
    */
   form!: FormGroup;
-   /**
-   * propiedad para mostrar mensajes de error solo si existe 
+  /**
+   * propiedad para mostrar mensajes de error solo si existe
    */
-  existeError: boolean = false; 
+  existeError: boolean = false;
   /**
    * propiedad que contiene el mensaje de error proveido por las validaciones del backend
    */
-  error:string = "";
-  
+  error: string = '';
+
   /**
    * propiedad para mostrar mensajes de exito solo si se realiza el cambio de constraseña exitosamente
    */
-  existemsgExito:boolean = false;
+  existemsgExito: boolean = false;
   /**
    * propiedad que contiene el mensaje de exito proveido por el backend solo si se realiza el cambio de constraseña exitosamente
    */
-  msgExito:string = "";
+  msgExito: string = '';
 
   /**
    * propiedad que contiene el nombre del personal que desea cambiar la contraseña
    */
-  nombreUsuario:string = "";
+  nombreUsuario: string = '';
 
   /**
    * inyeccion de dependencias
@@ -49,108 +49,120 @@ export class CambioContrasenaComponent implements OnInit {
     private QRUDService: QRUDService,
     private fb: FormBuilder,
     private authService: AuthService,
-    private ErrorServidor:ErrorServidorService,
-    private StorageService: StorageService
-  ) { }
+    private ErrorServidor: ErrorServidorService,
+    private StorageService: StorageService,
+    public translateHelper: DynamicTranslationsService
+  ) {}
 
   /**
    * inicializando el formulario reactivo y obteniendo el nombre del usuario
    */
   ngOnInit(): void {
-  this.cambiarContrasena()  
-  this.nombre();
-
-
+    this.cambiarContrasena();
+    this.nombre();
   }
   /**
    * metodo que inicializa el formulario reactivo con sus respectivos campos y validaciones
    */
-  cambiarContrasena(){
-    this.form = this.fb.group({
-      lastpwd:["",[Validators.required,Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)]],
-      newpwd:["",[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/g)]],
-      newpwd2:["",[Validators.required]]
-    },{validators:this.passwordsIguales("newpwd","newpwd2")})
+  cambiarContrasena() {
+    this.form = this.fb.group(
+      {
+        lastpwd: [
+          '',
+          [
+            Validators.required,
+            // Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)
+          ],
+        ],
+        newpwd: [
+          '',
+          [
+            Validators.required,
+            // Validators.pattern(
+            //   /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/g
+            // ),
+          ],
+        ],
+        newpwd2: ['', [Validators.required]],
+      },
+      { validators: this.passwordsIguales('newpwd', 'newpwd2') }
+    );
   }
-    /**
+  /**
    * metodo que se encarga de realizar el cambio de contraseña y enviar el mensaje de exito o error al usuario en caso de que se haya realizado o no
    */
-  submit(){
-    if(this.form.invalid){
+  submit() {
+    if (this.form.invalid) {
       this.form.markAllAsTouched();
       return;
     }
     const actualizarContrasena = this.form.value;
 
-    this.QRUDService.cambiarContrasena(actualizarContrasena).then((data:any) => {
-      this.msgExito = data.msg
-      this.form.reset();
-      this.existemsgExito = true;
+    this.QRUDService.cambiarContrasena(actualizarContrasena)
+      .then((data: any) => {
+        this.msgExito = data.msg;
+        this.form.reset();
+        this.existemsgExito = true;
 
-      setTimeout(() => {
-        this.existemsgExito = false;
-      },1500)
-
-    }).catch((err) => {
-
-      if(err.error.msg){
-        this.error = err.error.msg;
-        this.existeError = true;
-  
         setTimeout(() => {
-          this.existeError = false; 
-        },1500)
-        return;
-      }
+          this.existemsgExito = false;
+        }, 1500);
+      })
+      .catch((err) => {
+        if (err.error.msg) {
+          this.error = err.error.msg;
+          this.existeError = true;
 
-      if(err.error.msgtk){
-        this.authService.logout();
-        return;
-       }
-       
-       this.ErrorServidor.error();
+          setTimeout(() => {
+            this.existeError = false;
+          }, 1500);
+          return;
+        }
 
-    })
+        if (err.error.msgtk) {
+          this.authService.logout();
+          return;
+        }
 
- 
-
+        this.ErrorServidor.error();
+      });
   }
-/**
- * validacion personalizada que se encarga de validar que las contraseñas sean iguales
- */
-  passwordsIguales(pass1:string,pass2:string){
-    return (formGroup:FormGroup)=>{
+  /**
+   * validacion personalizada que se encarga de validar que las contraseñas sean iguales
+   */
+  passwordsIguales(pass1: string, pass2: string) {
+    return (formGroup: FormGroup) => {
       const pass1Control = formGroup.get(pass1);
       const pass2Control = formGroup.get(pass2);
-  
-      if(pass1Control?.value === pass2Control?.value){
+
+      if (pass1Control?.value === pass2Control?.value) {
         pass2Control?.setErrors(null);
-      }else{
-        pass2Control?.setErrors({noEsigual:true});
+      } else {
+        pass2Control?.setErrors({ noEsigual: true });
       }
-    }
+    };
   }
-    /**
+  /**
    * valida campos vacios del formulario reactivo si existen retorna un valor booleano true
    * @param campo recibe un campo del formulario para validar si contiene errores de validacion o no
    */
-  campoValido(campo:string){
-    return !this.form.get(campo)?.valid && this.form.get(campo)?.touched ;
+  campoValido(campo: string) {
+    return !this.form.get(campo)?.valid && this.form.get(campo)?.touched;
   }
 
-
-/**
- * metodo que remueve los mensajes de error solo si existen
- */
-  removerAlertas(){
+  /**
+   * metodo que remueve los mensajes de error solo si existen
+   */
+  removerAlertas() {
     this.existeError = false;
   }
   /**
-   * obtiene el nombre del usuario que desea cambiar la contraseña 
+   * obtiene el nombre del usuario que desea cambiar la contraseña
    */
-  nombre(){
-    this.nombreUsuario = this.StorageService.desencriptar("nombre");
-
+  nombre() {
+    this.nombreUsuario = this.StorageService.desencriptar('nombre');
   }
-
+  instantTranslation(key: string, params?: any) {
+    return this.translateHelper.instantTranslation(key, params);
+  }
 }
